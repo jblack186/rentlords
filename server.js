868372d13321ec4a43adf168b1534243c930dcbf
Jamison Blackwell <blackwellj1040@gmail.com>
2020-04-01 21:54:34 -0400

    test

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
 const pgp = require('pg-promise')()
 const server = express();
-const dbEnv = process.env.DB_ENV || 'development';
 
 
 
