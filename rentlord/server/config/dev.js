a55474e49bc2cc0e5c23349991092727b2a7dbc0
Jamison Blackwell <blackwellj1040@gmail.com>
2020-08-24 20:03:20 -0400

    add dev

---
+++ rentlord/server/config/dev.js
@@ -0,0 +1,7 @@
+//dev.js dont commit this
+module.exports = {
+    googleClientID: '61974376889-2io6ghepeqh9hfua79imqnn5d7a587ps.apps.googleusercontent.com',
+    googleClientSecret: 'fUs8hHYmTH_RI85qsqq80AS0',
+    mongoURI: 'mongodb+srv://jblack186:eAiwydcp3XuWz8yr@rentlords-tihr7.mongodb.net/test?retryWrites=true&w=majority',
+    cookieKey: 'eihfwihfehf239h92'
+};
